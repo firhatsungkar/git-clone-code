c51a6eceb76abf54932d5343c3acdf40f3daae55
Muhamad Firhat <muhamad.firhat@gmail.com>
2023-02-06 09:12:46 +0700

    Fix setup tool

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setup(
     version='1.0.0',
     description="CLI for automatically clone git repo in the custom dir structures.",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         'click',
     ],
