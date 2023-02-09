03e87a4d4e544c6a7adf0c3a922988ec1144b3ef
Muhamad Firhat <muhamad.firhat@gmail.com>
2023-02-09 09:44:43 +0700

    fixing setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="git-clone-code",
+    name="clc",
     version='1.0.0',
     description="CLI for automatically clone git repo in the custom dir structures.",
     packages=find_packages(),
